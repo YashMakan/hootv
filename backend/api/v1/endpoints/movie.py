ac6c53368cd3d74b71e2dc2ed58d8db81096e58c
YashMakan <yashmakan.work@gmail.com>
2023-12-06 22:29:06 +0530

    minor backend change

--- backend/api/v1/endpoints/movie.py
+++ backend/api/v1/endpoints/movie.py
@@ -36,32 +36,3 @@ async def fetch_movie(request: MovieIdRequest):
 async def fetch_movie(request: MovieIdRequest):
     result = movieSource().fetch_movie_credits(request.movie_id)
     return {"result": result}
-#
-# @router.get("/categories")
-# async def movie_trending():
-#     return {"ping": "pong!"}
-#
-#
-# @router.get("/search")
-# async def movie_search(term: str):
-#     movies = movieSource.search_movie(term)
-#     return {"result": movies}
-#
-#
-# @router.get("/info")
-# async def movie_info(movie_id: int):
-#     movie = movieSource.fetch_movie_info(movie_id)
-#     return {"result": movie}
-#
-#
-# @router.get("/check-availability")
-# async def movie_check_availability(term: str, limit: Optional[int]):
-#     movie_torrent_links = EzflixDaraSource.fetch_movie_torrents(term, limit=limit)
-#     return {"result": len(movie_torrent_links) > 0, "movie_torrent_links": movie_torrent_links}
-#
-#
-# @router.get("/fetch")
-# async def movie_torrent_bytes(torrent_url: str):
-#     torrent_bytes = EzflixDaraSource.fetch_torrent_bytes(torrent_url)
-#     # update in db
-#     return torrent_bytes
